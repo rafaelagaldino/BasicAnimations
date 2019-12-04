5f421341542c0025f5ddba3ee156b38888e21d67
rafaela galdino <rafaela.galdino@visionnaire.com.br>
2019-12-04 16:31:33 -0300

    first commit

---
+++ StudyAnimate/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  StudyAnimate
+//
+//  Created by Rafaela Galdino on 04/12/19.
+//  Copyright © 2019 Rafaela Galdino. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
+
